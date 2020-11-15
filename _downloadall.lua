0e57c71233b4c61568b9da525c85bf7d9290da89
Cynthia Foxwell <lumodder@gmail.com>
2020-11-14 19:32:14 -0700

    ease of use dl file

---
+++ _downloadall.lua
@@ -0,0 +1,24 @@
+local cmd = "wget" -- "nget"
+local baseURL = "https://raw.githubusercontent.com/Cynosphere/lunatic86/master/"
+
+local files = {
+    "comms",
+    "config_disks",
+    "disks",
+    "emu_core",
+    "kbdmaps",
+    "keyboard",
+    "lunatic86",
+    "pic",
+    "pit",
+    "platform_oc",
+    "sysconf",
+    "table_cp437",
+    "table_ocpalette",
+    "timer",
+    "video"
+}
+
+for _, f in ipairs(files) do
+    shell.execute(string.format("%s %s%s.lua", cmd, baseURL, f))
+end
